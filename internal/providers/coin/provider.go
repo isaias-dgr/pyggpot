a8d28e2a26135d57af98c3dbd8bf1cd7ce09e6a6
Isaias G <isda.garo@gmail.com>
2022-07-15 00:11:24 -0500

    add remove coins endpoint (validation message for create pot)

--- internal/providers/coin/provider.go
+++ internal/providers/coin/provider.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/aspiration-labs/pyggpot/internal/models"
-	"github.com/aspiration-labs/pyggpot/rpc/go/coin"
+	coin_service "github.com/aspiration-labs/pyggpot/rpc/go/coin"
 	"github.com/twitchtv/twirp"
 )
 
-type coinServer struct{
+type coinServer struct {
 	DB *sql.DB
 }
 
@@ -31,9 +34,9 @@ func (s *coinServer) AddCoins(ctx context.Context, request *coin_service.AddCoin
 	for _, coin := range request.Coins {
 		fmt.Println(coin)
 		newCoin := models.Coin{
-			PotID: request.PotId,
+			PotID:        request.PotId,
 			Denomination: int32(coin.Kind),
-			CoinCount: coin.Count,
+			CoinCount:    coin.Count,
 		}
 		err = newCoin.Save(tx)
 		if err != nil {
@@ -50,6 +53,66 @@ func (s *coinServer) AddCoins(ctx context.Context, request *coin_service.AddCoin
 	}, nil
 }
 
-func (s *coinServer) RemoveCoins(context.Context, *coin_service.RemoveCoinsRequest) (*coin_service.CoinsListResponse, error) {
-	panic("implement me")
+func (s *coinServer) RemoveCoins(ctx context.Context, request *coin_service.RemoveCoinsRequest) (*coin_service.CoinsListResponse, error) {
+	rand.Seed(time.Now().UnixNano())
+	if err := request.Validate(); err != nil {
+		return nil, twirp.InvalidArgumentError(err.Error(), "")
+	}
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return nil, twirp.InternalError(err.Error())
+	}
+
+	coins := []*coin_service.Coins{}
+	coinsInPot, err := models.CoinsInPotsByPot_id(tx, int(request.PotId))
+	if err != nil {
+		return nil, twirp.InternalError(err.Error())
+	}
+
+	rand.Shuffle(len(coinsInPot), func(i, j int) { coinsInPot[i], coinsInPot[j] = coinsInPot[j], coinsInPot[i] })
+	total := request.Count
+	for _, c := range coinsInPot {
+		coin, err := models.CoinByID(tx, c.ID)
+		if err != nil {
+			return nil, twirp.InternalError(err.Error())
+		}
+		if total == 0 {
+			break
+		}
+		count := total
+		if total > c.CoinCount {
+			coin.CoinCount = 0
+			count = c.CoinCount
+			total = total - c.CoinCount
+		} else {
+			coin.CoinCount = c.CoinCount - total
+			total = 0
+		}
+
+		coins = append(coins, &coin_service.Coins{
+			Kind:  coin_service.Coins_Kind(c.Denomination),
+			Count: count,
+		})
+		coins = append(coins, &coin_service.Coins{
+			Kind:  coin_service.Coins_Kind(c.Denomination),
+			Count: count,
+		})
+		if coin.CoinCount == 0 {
+			if err := coin.Delete(tx); err != nil {
+				return nil, twirp.InternalError(err.Error())
+			}
+		} else {
+			if err := coin.Update(tx); err != nil {
+				return nil, twirp.InternalError(err.Error())
+			}
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		return nil, twirp.NotFoundError(err.Error())
+	}
+
+	return &coin_service.CoinsListResponse{Coins: coins}, nil
+
 }
